bb71eefc2b18598fa5ca20807ed48db64ecd416b
Hamed Ahmed <hamedahmedhamed100@gmail.com>
2026-03-11 02:04:25 +0100

    s3

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -62,9 +62,31 @@ public static class DependencyInjection
         builder.Services.AddTransient<IIdentityService, IdentityService>();
         builder.Services.AddScoped<IUserLookupService, UserLookupService>();
         builder.Services.AddSingleton<IInviteCodeService, InviteCodeService>();
-        builder.Services.AddScoped<IImageUploadService, LocalImageUploadService>();
+
+        var isProduction = builder.Environment.IsProduction();
+        if (isProduction)
+        {
+            builder.Services.Configure<S3StorageOptions>(options =>
+            {
+                options.AccessKey = builder.Configuration["S3_ACCESS_KEY"] ?? string.Empty;
+                options.SecretKey = builder.Configuration["S3_SECRET_KEY"] ?? string.Empty;
+                options.Bucket = builder.Configuration["S3_BUCKET"] ?? "photos";
+                options.Endpoint = builder.Configuration["S3_ENDPOINT"] ?? "https://eu2.contabostorage.com";
+                options.PublicBaseUrl = builder.Configuration["S3_PUBLIC_BASE_URL"];
+                options.ForcePathStyle = true;
+            });
+
+            builder.Services.AddScoped<IObjectStorageService, S3ObjectStorageService>();
+            builder.Services.AddScoped<IImageUploadService, S3ImageUploadService>();
+            builder.Services.AddScoped<IMediaLibraryFileService, S3MediaLibraryFileService>();
+        }
+        else
+        {
+            builder.Services.AddScoped<IImageUploadService, LocalImageUploadService>();
+            builder.Services.AddScoped<IMediaLibraryFileService, LocalMediaLibraryFileService>();
+        }
+
         builder.Services.AddScoped<IGroupPricingService, GroupPricingService>();
-        builder.Services.AddScoped<IMediaLibraryFileService, LocalMediaLibraryFileService>();
 
         // Register HttpClient for Fatorah payment service
         builder.Services.AddHttpClient(nameof(FatorahPaymentService));
